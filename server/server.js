f8aa1322b338e4af597433b79afda2d9d4251914
Gaurav Goswami <gauravgoswami.aug@gmail.com>
2023-07-22 08:10:35 +0530

    created basic structure of client and server

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+
+dotenv.config({
+    path : "./config/config.env"
+})
+
+const app = express()
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`Server is running at PORT ${PORT}`)
+})
