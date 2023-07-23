5e802db7623b57015fb4cd05ddfe4b8674740fee
Gaurav Goswami <gauravgoswami.aug@gmail.com>
2023-07-23 23:25:56 +0530

    created farmer education page

--- client/src/data/educationData.js
+++ client/src/data/educationData.js
@@ -1,26 +1,28 @@
 const education = [
     {
-        link : "https://youtu.be/wougJaN_Ha0",
+        link : "https://www.youtube.com/embed/wougJaN_Ha0",
         title : "What is Organic Agriculture"
     },
     {
-        link : "https://youtu.be/AqQ9nM5JIes",
+        link : "https://www.youtube.com/embed/AqQ9nM5JIes",
         title : "Farming In USA"
     },
     {
-        link : "https://youtu.be/oQlicIzQork",
+        link : "https://youtube.com/embed/oQlicIzQork",
         title : "Farm Bill 2020"
     },
     {
-        link : "https://youtu.be/I_RcKtH4XyE",
+        link : "https://youtube.com/embed/I_RcKtH4XyE",
         title : "Minimum Suport Price"
     },
     {
-        link : "https://youtu.be/3EUL_f8vGJ0",
+        link : "https://youtube.com/embed/3EUL_f8vGJ0",
         title : "Agriculture Reform Bill 2020"
     },
     {
-        link : "https://youtu.be/z5J-CDBpNZE",
+        link : "https://youtube.com/embed/z5J-CDBpNZE",
         title : "Farmers Producer Organisations in India"
     }
 ]
+
+export default education
--- client/src/pages/EducationPage.jsx
+++ client/src/pages/EducationPage.jsx
@@ -1,11 +1,34 @@
 import React from "react";
 import MainWrapper from "../Wrapper/MainWrapper";
+import education from "../data/educationData";
 
 const EducationPage = () => {
   return (
     <>
       <MainWrapper>
-        <h1>This is education page</h1>
+        <div className="w-[95%] md:w-[85%] lg:w-[70%] xl:w-[1300px]  bg-slate-100 mx-auto flex flex-col gap-4 flex-wrap">
+          <h2 className="text-center text-2xl md:text-5xl font-bold text-green-500">
+            Farmer Education
+          </h2>
+          <div className="flex flex-wrap gap-2 justify-center">
+            {education.map((item, index) => {
+              return (
+                <div className="w-[100%] md:w-[45%] p-1 bg-green h-[350px]" key={index}>
+                  <iframe
+                    width="560"
+                    height="315"
+                    src={item.link}
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowfullscreen
+                    className="w-[100%] h-[100%]"
+                  ></iframe>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </MainWrapper>
     </>
   );
