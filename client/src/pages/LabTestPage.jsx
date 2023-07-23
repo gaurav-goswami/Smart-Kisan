5b750de4033d28e0d0c82ce32bb9067536730d30
Gaurav Goswami <gauravgoswami.aug@gmail.com>
2023-07-24 01:19:40 +0530

    added lab test page

--- client/src/pages/LabTestPage.jsx
+++ client/src/pages/LabTestPage.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 import MainWrapper from "../Wrapper/MainWrapper";
+import cropLabTest from "../assets/cropLabTest.jpg";
+import { LAB_MESSAGE } from "../data/messages";
+
 
 const LabTestPage = () => {
   return (
     <>
       <MainWrapper>
-        <h1>This is Lab test page</h1>
+        <div className="w-[95%] min-h-screen md:w-[80%] p-2 mx-auto flex flex-col gap-5">
+          <h2 className="text-2xl md:text-5xl font-bold text-center">Lab Test</h2>
+
+          <div className="w-[100%] flex flex-col md:flex-row">
+            <div className="w-[100%] lg:w-[50%] h-[400px] p-4 mt-2">
+              <img src={cropLabTest} alt="crop lab test image" className="h-full w-full object-cover object-center" />
+            </div>
+
+            <p className="w-[100%] lg:w-[50%] p-4 text-lg md:text-xl font-semibold text-gray-700">{LAB_MESSAGE}</p>
+
+          </div>
+
+          <button className='bg-yellow-400 px-6 py-3 rounded-lg font-semibold'>Book a test</button>
+          
+        </div>
       </MainWrapper>
     </>
   );
